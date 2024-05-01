e963951c7f3a9c66968d0db1699fd90ccd0ec88c
MrHotCakes0 <95427411+Goomba142@users.noreply.github.com>
2024-05-01 15:32:44 -0400

    Add files via upload

---
+++ wordlermain.py
@@ -0,0 +1,36 @@
+import datetime
+import requests
+
+# Get current date
+current_date = datetime.datetime.now()
+
+# Extract day, month, and year
+day = current_date.day
+month = current_date.month
+year = current_date.year
+
+if month < 10:
+    month_str = f"0{month}"
+else:
+    month_str = str(month)
+
+# Construct the link using variables
+link = f"https://nytimes.com/svc/wordle/v2/{year}-{month_str}-{day}.json"
+
+# Fetch data from the link
+response = requests.get(link)
+
+# Check if request was successful
+if response.status_code == 200:
+    data = response.json()
+
+    # Extract the solution word
+    try:
+        solution = data['solution']
+        print("The wordle answer for today is:", solution)
+    except KeyError:
+        print("No word data found in the response.")
+else:
+    print("Failed to fetch Wordle data from the server.")
+
+input(" ")
